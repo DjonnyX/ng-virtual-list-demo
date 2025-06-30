bdc5db253b2377d79eb475c62bef44bf27490fe5
Evgenii Grebennikov <djonnyx@gmail.com>
2025-06-30 23:48:31 +0300

    reverted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, viewChild } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
-import { NgVirtualListComponent, IScrollEvent, IRect } from '../../../virtual-list/projects/ng-virtual-list/src/public-api';
-// import { NgVirtualListComponent, IScrollEvent, IRect } from 'ng-virtual-list';
+// import { NgVirtualListComponent, IScrollEvent, IRect } from '../../../virtual-list/projects/ng-virtual-list/src/public-api';
+import { NgVirtualListComponent, IScrollEvent, IRect } from 'ng-virtual-list';
 import { BehaviorSubject, combineLatest, debounceTime, delay, filter, from, interval, map, mergeMap, of, switchMap, tap } from 'rxjs';
 import { LOGO } from './const';
 import { GROUP_DYNAMIC_ITEMS, GROUP_DYNAMIC_ITEMS_STICKY_MAP } from './const/collection';
@@ -20,7 +20,7 @@ const SNAP_HEIGHT = 100;
 export class AppComponent {
   readonly logo = LOGO;
 
-  protected _listContainerRef = viewChild('dynamicList', { read: NgVirtualListComponent });
+  protected _listContainerRef = viewChild('dynamicList', { read: ElementRef });
 
   private _$isEndOfListPosition = new BehaviorSubject<boolean>(true);
   readonly $isEndOfListPosition = this._$isEndOfListPosition;
@@ -34,70 +34,70 @@ export class AppComponent {
   constructor() {
     const list = this._listContainerRef;
 
-    const $virtualList = toObservable(list).pipe(
-      filter(list => !!list),
-      switchMap(list => combineLatest([of(list), list?.$initialized])),
-      filter(([, init]) => !!init),
-      map(([list]) => list),
-    );
+    // const $virtualList = toObservable(list).pipe(
+    //   filter(list => !!list),
+    //   switchMap(list => combineLatest([of(list), list?.$initialized])),
+    //   filter(([, init]) => !!init),
+    //   map(([list]) => list),
+    // );
 
-    combineLatest([this.$version, $virtualList]).pipe(
-      map(([version, list]) => ({ version, list })),
-      mergeMap(({ version, list }) => {
-        return combineLatest([of(version), of(list)]);
-      }),
-      map(([version, list]) => ({ version, list })),
-      filter(({ list }) => !!list),
-      debounceTime(50),
-      tap(({ version, list }) => {
-        if (version === 0) {
-          list!.scrollToEnd('instant');
-        }
+    // combineLatest([this.$version, $virtualList]).pipe(
+    //   map(([version, list]) => ({ version, list })),
+    //   mergeMap(({ version, list }) => {
+    //     return combineLatest([of(version), of(list)]);
+    //   }),
+    //   map(([version, list]) => ({ version, list })),
+    //   filter(({ list }) => !!list),
+    //   debounceTime(50),
+    //   tap(({ version, list }) => {
+    //     if (version === 0) {
+    //       list!.scrollToEnd('instant');
+    //     }
 
-        if (this._$isEndOfListPosition.getValue()) {
-          list!.scrollToEnd('instant');
-        }
-      })
-    ).subscribe();
+    //     if (this._$isEndOfListPosition.getValue()) {
+    //       list!.scrollToEnd('instant');
+    //     }
+    //   })
+    // ).subscribe();
 
-    $virtualList.pipe(
-      delay(100),
-      switchMap(() => this.write()),
-    ).subscribe();
+    // $virtualList.pipe(
+    //   delay(100),
+    //   switchMap(() => this.write()),
+    // ).subscribe();
 
-    from(interval(2000)).pipe(
-      switchMap(() => this.write()),
-    ).subscribe();
+    // from(interval(2000)).pipe(
+    //   switchMap(() => this.write()),
+    // ).subscribe();
 
-    combineLatest([this.$scrollParams, $virtualList, this.$version]).pipe(
-      delay(10),
-      switchMap(([{ viewportEndY, scrollWeight }, list]) => {
-        let bounds: IRect | undefined;
-        if (list) {
-          bounds = list.getItemBounds(this.groupDynamicItems[this.groupDynamicItems.length - 1].id);
-        }
+    // combineLatest([this.$scrollParams, $virtualList, this.$version]).pipe(
+    //   delay(10),
+    //   switchMap(([{ viewportEndY, scrollWeight }, list]) => {
+    //     let bounds: IRect | undefined;
+    //     if (list) {
+    //       bounds = list.getItemBounds(this.groupDynamicItems[this.groupDynamicItems.length - 1].id);
+    //     }
 
-        const height = (bounds?.height ?? 0);
+    //     const height = (bounds?.height ?? 0);
 
-        return of((viewportEndY + height + SNAP_HEIGHT) >= scrollWeight);
-      }),
-      tap(v => {
-        this._$isEndOfListPosition.next(v);
-      }),
-    ).subscribe();
+    //     return of((viewportEndY + height + SNAP_HEIGHT) >= scrollWeight);
+    //   }),
+    //   tap(v => {
+    //     this._$isEndOfListPosition.next(v);
+    //   }),
+    // ).subscribe();
 
-    const appHeightHandler = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
-    window.addEventListener('resize', appHeightHandler);
+    // const appHeightHandler = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
+    // window.addEventListener('resize', appHeightHandler);
 
-    $virtualList.pipe(
-      tap(() => {
-        appHeightHandler();
-      }),
-      delay(100),
-      tap(() => {
-        document.documentElement.style.setProperty('--viewport-alpha', '1');
-      }),
-    ).subscribe();
+    // $virtualList.pipe(
+    //   tap(() => {
+    //     appHeightHandler();
+    //   }),
+    //   delay(100),
+    //   tap(() => {
+    //     document.documentElement.style.setProperty('--viewport-alpha', '1');
+    //   }),
+    // ).subscribe();
   }
 
   private write() {
@@ -117,7 +117,7 @@ export class AppComponent {
         items.push(msg);
         this.groupDynamicItemsStickyMap[msg.id] = 0;
 
-        for (let i = 0, l = 1; i < l; i++) {
+        for (let i = 0, l = 50; i < l; i++) {
           const msgStart = generateMessage(this.groupDynamicItems.length + 10000 + i);
           this.groupDynamicItemsStickyMap[msgStart.id] = 0;
           items.unshift(msgStart);
@@ -138,16 +138,16 @@ export class AppComponent {
   readonly $scrollParams = this._$scrollParams.asObservable();
 
   onScrollHandler(e: IScrollEvent & { [x: string]: any; }) {
-    this._$scrollParams.next({
-      viewportEndY: e.scrollSize + e.size,
-      scrollWeight: e.scrollWeight,
-    });
+    // this._$scrollParams.next({
+    //   viewportEndY: e.scrollSize + e.size,
+    //   scrollWeight: e.scrollWeight,
+    // });
   }
 
   onScrollEndHandler(e: IScrollEvent & { [x: string]: any; }) {
-    this._$scrollParams.next({
-      viewportEndY: e.scrollSize + e.size,
-      scrollWeight: e.scrollWeight,
-    });
+    // this._$scrollParams.next({
+    //   viewportEndY: e.scrollSize + e.size,
+    //   scrollWeight: e.scrollWeight,
+    // });
   }
 }
